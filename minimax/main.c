7179fadcbebb351b358dbd1321bdd1b6ab5aa5f2
Priyanuj Bora <while.alive.program@gmail.com>
2024-04-10 22:31:09 +0530

    Implementing Minimax

---
+++ minimax/main.c
@@ -0,0 +1,11 @@
+// NOTE: the following program uses some concepts of heap
+
+
+#include<stdio.h>
+
+int main(void) {
+	int scores[] = {3, 5, 2, 9, 12, 5, 23, 23};
+	int n = 8; // 8 elements are there in this array
+
+	return 0;
+}
