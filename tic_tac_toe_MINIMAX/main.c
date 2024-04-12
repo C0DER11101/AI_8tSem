73d3ac8f7c8aeeb8d3227e3de750bf641846bdb7
Priyanuj Bora <while.alive.program@gmail.com>
2024-04-12 21:45:40 +0530

    tic-tac-toe using minimax

--- tic_tac_toe_MINIMAX/main.c
+++ tic_tac_toe_MINIMAX/main.c
@@ -10,6 +10,27 @@ int main(void) {
 
 	for(int i = 0; i < 3; i++)
 		for(int j = 0; j < 3; j++)
-			board[i][j] = 95;
+			board[i][j] = '_';
+	int scores[] = {-1, 0, 1};
+	/* -1 -> machine wins the game
+	   0  -> tie
+	   1  -> user wins the game
+	 */
+	
+	int xPos, yPos;
+
+	while(1) {
+
+		printf("user: ");
+		scanf("%d%d", &xPos, &yPos);
+
+		if((xPos > 0 && xPos < 3) || (yPos > 0 && yPos < 3)) {
+			puts("Invalid coordinates!");
+			continue;
+		}
+
+		board[xPos][yPos] = 'x';
+	}
+
 	return 0;
 }
