3b60c701ca395192a61cb843c10687e3bd0659a8
Priyanuj Bora <while.alive.program@gmail.com>
2024-04-14 20:02:25 +0530

    Tic-Tac-Toe using Minimax

--- tic_tac_toe_MINIMAX/bestMove.c
+++ tic_tac_toe_MINIMAX/bestMove.c
@@ -10,9 +10,9 @@ void bestPos(int*board) {
 	int move;
 	int bestScore = -INF;
 	int score;
-	for(int i = 0; i < 9; i++) {
+	for(int i = 0; i < 9; i++) { // to find the best move, we need to first check if the board has empty space in it
 		if(board[i] == '_') { // checking for an empty space
-			board[i] = ai;
+			board[i] = ai; // temporarily place AI's symbol in that empty space
 			score = minimax(board, 0, false); // we are the maximizing player
 			board[i] = '_'; // restoring the position
 			if(score > bestScore) { // set the bestScore to the score that is greater than bestScore
