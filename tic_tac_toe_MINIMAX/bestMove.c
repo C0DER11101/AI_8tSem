918566476eaf1f00049ce3a70ed45e8e11708c93
Priyanuj Bora <while.alive.program@gmail.com>
2024-05-30 06:33:30 +0530

    Tic-Tac-Toe using Minimax

--- tic_tac_toe_MINIMAX/bestMove.c
+++ tic_tac_toe_MINIMAX/bestMove.c
@@ -24,7 +24,7 @@ void bestPos(int*board) {
 			board[i] = '_'; // restoring the position
 			if(score > bestScore) { // set the bestScore to the score that is greater than bestScore
 				bestScore = score;
-				move = i; // if we get a new best score that the position where we got that new best score is the best position
+				move = i; // if we get a new best score then the position where we got that new best score is the best position
 			}
 		}
 	}
